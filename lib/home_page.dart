1be4fe5fbe9ffd371aa2c6d29abaa5a65a8c65ab
PatienceA26 <afiapatienceb@gmail.com>
2021-11-23 16:36:39 +0000

    added floating action button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:contact_application/contact_view.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,67 +9,126 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: false,
-          title: Text('My Contact',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.w600)),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('aaset/africa.jpg'),
-                radius: 30.0,
-              ),
-            )
-          ],
-          bottom: PreferredSize(
-              preferredSize: Size.fromHeight(90),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    hintText: 'search by name or number',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                  ),
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            centerTitle: false,
+            title: Text('My Contact',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w600)
+                    ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('aaset/africa.jpg'),
+                  radius: 30.0,
                 ),
-              ))),
-      body: SafeArea(
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: Text(
-                  'Recent',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-                )),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage('aaset/africa.jpg'),
+              )
+            ],
+            bottom: PreferredSize(
+                preferredSize: Size.fromHeight(90),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'search by name or number',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ))),
+        body: SafeArea(
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              const Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Text(
+                    'Recent',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  )),
+              ListView.separated(
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return const ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('aaset/africa.jpg'),
+                    ),
+                    title: Text(
+                      'Nana Kwasi',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.w600),
+                    ),
+                    subtitle: Text('+233 54 151 9532'),
+                    trailing: Icon(
+                      Icons.more_horiz,
+                      size: 20,
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider();
+                },
+                itemCount: 3,
               ),
-              title: Text('Francis Agyei'),
-              subtitle: Text('+233 54 151 9532'),
-              trailing: Icon(Icons.more_horiz),
-            )
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage('aaset/africa.jpg'),
-              ),
-              title: Text('Francis Agyei'),
-              subtitle: Text('+233 54 151 9532'),
-              trailing: Icon(Icons.more_horiz),
-            )
-          ],
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Text('Contact',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w600))),
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Text('A',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w600))),
+              ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return ContactView();
+                        }));
+                      },
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage('aaset/africa.jpg'),
+                      ),
+                      title: Text(
+                        'Francis Agyei',
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600),
+                      ),
+                      subtitle: Text('+233 54 151 9532'),
+                      trailing: Icon(
+                        Icons.more_horiz,
+                        size: 20,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 8,
+                    );
+                  },
+                  itemCount: 2)
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            size: 30,
+          ),
+          onPressed: () {},
+          backgroundColor: Colors.black,
+        ));
   }
 }
